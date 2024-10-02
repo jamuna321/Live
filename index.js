f6a1e5b76dc7741c0fa0c521ae5cb25115611f18
jamuna321 <jamunadahal321@gmail.com>
2024-10-02 18:34:20 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,504 @@
+var data ={
+    "SuperSport": [
+        {
+            "channel_name": "SuperSport Premier league",
+            "link": "https://dlhd.so/embed/stream-414.php",
+        },
+        {
+            "channel_name": "SuperSport Football",
+            "link": "https://dlhd.so/embed/stream-56.php",
+        },
+        {
+            "channel_name": "SuperSport LaLiga",
+            "link": "https://dlhd.so/embed/stream-415.php",
+        },
+        {
+            "channel_name": "SuperSport Variety 1",
+            "link": "https://dlhd.so/embed/stream-416.php",
+        },
+        {
+            "channel_name": "SuperSport Variety 2",
+            "link": "https://dlhd.so/embed/stream-417.php",
+        },
+        {
+            "channel_name": "SuperSport Variety 3",
+            "link": "https://dlhd.so/embed/stream-418.php",
+        },
+        {
+            "channel_name": "SuperSport Variety 4",
+            "link": "https://dlhd.so/embed/stream-419.php",
+        },
+        {
+            "channel_name": "SuperSport Action",
+            "link": "https://dlhd.so/embed/stream-420.php",
+        },
+        {
+            "channel_name": "SuperSport Cricket",
+            "link": "https://dlhd.so/embed/stream-368.php",
+        },
+        {
+            "channel_name": "SuperSport Golf",
+            "link": "https://dlhd.so/embed/stream-422.php",
+        },
+        {
+            "channel_name": "SuperSport Tennis",
+            "link": "https://dlhd.so/embed/stream-423.php",
+        },
+        {
+            "channel_name": "SuperSport Grandstand",
+            "link": "https://dlhd.so/embed/stream-412.php",
+        },
+        {
+            "channel_name": "SuperSport PSL",
+            "link": "https://dlhd.so/embed/stream-413.php",
+        },
+        {
+            "channel_name": "SuperSport Rugby",
+            "link": "https://dlhd.so/embed/stream-421.php",
+        },
+        {
+            "channel_name": "SuperSport Motorsport",
+            "link": "https://dlhd.so/embed/stream-424.php",
+        },
+        {
+            "channel_name": "SuperSport MaXimo 1",
+            "link": "https://dlhd.so/embed/stream-572.php",
+        }
+    ],
+    "Sky Sports": [
+        {
+            "channel_name": "Sky Sports Main Event",
+            "link": "https://dlhd.so/embed/stream-38.php",
+        },
+        {
+            "channel_name": "Sky Sports Premier League",
+            "link": "https://dlhd.so/embed/stream-130.php",
+        },
+        {
+            "channel_name": "Sky Sports Football",
+            "link": "https://dlhd.so/embed/stream-35.php",
+        },
+        {
+            "channel_name": "Sky Sports Cricket",
+            "link": "https://dlhd.so/embed/stream-65.php",
+        },
+        {
+            "channel_name": "Sky Sports Golf",
+            "link": "https://dlhd.so/embed/stream-70.php",
+        },
+        {
+            "channel_name": "Sky Sports Action",
+            "link": "https://dlhd.so/embed/stream-37.php",
+        },
+        {
+            "channel_name": "Sky Sports Arena",
+            "link": "https://dlhd.so/embed/stream-36.php",
+        },
+        {
+            "channel_name": "Sky Sports F1",
+            "link": "https://dlhd.so/embed/stream-60.php",
+        },
+        {
+            "channel_name": "Sky Sports Racing",
+            "link": "https://dlhd.so/embed/stream-554.php",
+        },
+        {
+            "channel_name": "Sky Sport 1 NZ",
+            "link": "https://dlhd.so/embed/stream-588.php",
+        },
+        {
+            "channel_name": "Sky Sport 2 NZ",
+            "link": "https://dlhd.so/embed/stream-589.php",
+        },
+        {
+            "channel_name": "Sky Sport 3 NZ",
+            "link": "https://dlhd.so/embed/stream-590.php",
+        },
+        {
+            "channel_name": "Sky Sport 4 NZ",
+            "link": "https://dlhd.so/embed/stream-591.php",
+        },
+        {
+            "channel_name": "Sky Sport 5 NZ",
+            "link": "https://dlhd.so/embed/stream-592.php",
+        },
+        {
+            "channel_name": "Sky Sport 6 NZ",
+            "link": "https://dlhd.so/embed/stream-593.php",
+        },
+        {
+            "channel_name": "Sky Sport 7 NZ",
+            "link": "https://dlhd.so/embed/stream-594.php",
+        },
+        {
+            "channel_name": "Sky Sport 8 NZ",
+            "link": "https://dlhd.so/embed/stream-595.php",
+        },
+        {
+            "channel_name": "Sky Sport 9 NZ",
+            "link": "https://dlhd.so/embed/stream-596.php",
+        },
+        {
+            "channel_name": "Sky Sport Select NZ",
+            "link": "https://dlhd.so/embed/stream-587.php",
+        }
+    ],
+    "TNT Sports": [
+        {
+            "channel_name": "TNT Sports 1",
+            "link": "https://dlhd.so/embed/stream-31.php",
+        },
+        {
+            "channel_name": "TNT Sports 2",
+            "link": "https://dlhd.so/embed/stream-32.php",
+        },
+        {
+            "channel_name": "TNT Sports 3",
+            "link": "https://dlhd.so/embed/stream-33.php",
+        },
+        {
+            "channel_name": "TNT Sports 4",
+            "link": "https://dlhd.so/embed/stream-34.php",
+        },
+        {
+            "channel_name": "TNT USA",
+            "link": "https://dlhd.so/embed/stream-338.php",
+        }
+    ],
+    "Fox Sports": [
+        {
+            "channel_name": "Fox Sports 1 USA",
+            "link": "https://dlhd.so/embed/stream-39.php",
+        },
+        {
+            "channel_name": "Fox Sports 2 USA",
+            "link": "https://dlhd.so/embed/stream-758.php",
+        },
+        {
+            "channel_name": "Fox Cricket",
+            "link": "https://dlhd.so/embed/stream-369.php",
+        },
+        {
+            "channel_name": "FOX Soccer Plus",
+            "link": "https://dlhd.so/embed/stream-756.php",
+        }
+    ],
+    "TNS": [
+        {
+            "channel_name": "TSN1",
+            "link": "https://dlhd.so/embed/stream-111.php",
+        },
+        {
+            "channel_name": "TSN2",
+            "link": "https://dlhd.so/embed/stream-112.php",
+        },
+        {
+            "channel_name": "TSN3",
+            "link": "https://dlhd.so/embed/stream-113.php",
+        },
+        {
+            "channel_name": "TSN4",
+            "link": "https://dlhd.so/embed/stream-114.php",
+        },
+        {
+            "channel_name": "TSN5",
+            "link": "https://dlhd.so/embed/stream-115.php",
+        }
+    ],
+    "Star Sports": [
+        {
+            "channel_name": "Star Sport 1 HD",
+            "link": "https://dlhd.so/embed/stream-267.php",
+        },
+        {
+            "channel_name": "Star Sport 1 Hindi HD",
+            "link": "https://dlhd.so/embed/stream-268.php",
+        }
+    ],
+    "Sony Sports": [
+        {
+            "channel_name": "Sony Ten 1 HD",
+            "link": "https://dlhd.so/embed/stream-885.php",
+        },
+        {
+            "channel_name": "Sony Ten 2 HD",
+            "link": "https://dlhd.so/embed/stream-886.php",
+        },
+        {
+            "channel_name": "Sony Ten 3 HD",
+            "link": "https://dlhd.so/embed/stream-887.php",
+        },
+        {
+            "channel_name": "Sony Ten 4 HD",
+            "link": "https://dlhd.so/embed/stream-888.php",
+        },
+        {
+            "channel_name": "Sony Ten 5 HD",
+            "link": "https://dlhd.so/embed/stream-889.php",
+        }
+    ],
+    "Astro Sports": [
+        {
+            "channel_name": "Astro SuperSport 1",
+            "link": "https://dlhd.so/embed/stream-123.php",
+        },
+        {
+            "channel_name": "Astro SuperSport 2",
+            "link": "https://dlhd.so/embed/stream-124.php",
+        },
+        {
+            "channel_name": "Astro SuperSport 3",
+            "link": "https://dlhd.so/embed/stream-125.php",
+        },
+        {
+            "channel_name": "Astro SuperSport 4",
+            "link": "https://dlhd.so/embed/stream-126.php",
+        },
+        {
+            "channel_name": "Astro Cricket",
+            "link": "https://dlhd.so/embed/stream-370.php",
+        }
+    ],
+    "Bein Sports": [
+        {
+            "channel_name": "beIN Sports MENA English 1",
+            "link": "https://dlhd.so/embed/stream-61.php",
+        },
+        {
+            "channel_name": "beIN Sports MENA English 2",
+            "link": "https://dlhd.so/embed/stream-90.php",
+        },
+        {
+            "channel_name": "beIN Sports MENA English 3",
+            "link": "https://dlhd.so/embed/stream-46.php",
+        }
+    ],
+    "Adults": [
+        {
+            "channel_name": "Stream 01",
+            "link": "https://dlhd.so/embed/stream-501.php",
+        },
+        {
+            "channel_name": "Stream 02",
+            "link": "https://dlhd.so/embed/stream-502.php",
+        },
+        {
+            "channel_name": "Stream 03",
+            "link": "https://dlhd.so/embed/stream-503.php",
+        },
+        {
+            "channel_name": "Stream 04",
+            "link": "https://dlhd.so/embed/stream-504.php",
+        },
+        {
+            "channel_name": "Stream 05",
+            "link": "https://dlhd.so/embed/stream-505.php",
+        },
+        {
+            "channel_name": "Stream 06",
+            "link": "https://dlhd.so/embed/stream-506.php",
+        },
+        {
+            "channel_name": "Stream 07",
+            "link": "https://dlhd.so/embed/stream-507.php",
+        },
+        {
+            "channel_name": "Stream 08",
+            "link": "https://dlhd.so/embed/stream-508.php",
+        },
+        {
+            "channel_name": "Stream 09",
+            "link": "https://dlhd.so/embed/stream-509.php",
+        },
+        {
+            "channel_name": "Stream 10",
+            "link": "https://dlhd.so/embed/stream-510.php",
+        },
+        {
+            "channel_name": "Stream 11",
+            "link": "https://dlhd.so/embed/stream-511.php",
+        },
+        {
+            "channel_name": "Stream 12",
+            "link": "https://dlhd.so/embed/stream-512.php",
+        },
+        {
+            "channel_name": "Stream 13",
+            "link": "https://dlhd.so/embed/stream-513.php",
+        },
+        {
+            "channel_name": "Stream 14",
+            "link": "https://dlhd.so/embed/stream-514.php",
+        },
+        {
+            "channel_name": "Stream 15",
+            "link": "https://dlhd.so/embed/stream-515.php",
+        },
+        {
+            "channel_name": "Stream 16",
+            "link": "https://dlhd.so/embed/stream-516.php",
+        },
+        {
+            "channel_name": "Stream 17",
+            "link": "https://dlhd.so/embed/stream-517.php",
+        },
+        {
+            "channel_name": "Stream 18",
+            "link": "https://dlhd.so/embed/stream-518.php",
+        },
+        {
+            "channel_name": "Stream 19",
+            "link": "https://dlhd.so/embed/stream-519.php",
+        },
+        {
+            "channel_name": "Stream 20",
+            "link": "https://dlhd.so/embed/stream-520.php",
+        }
+    ],
+    "Football Clubs": [
+        {
+            "channel_name": "Liverpool TV",
+            "link": "https://dlhd.so/embed/stream-826.php",
+        },
+        {
+            "channel_name": "Real Madrid TV",
+            "link": "https://dlhd.so/embed/stream-523.php",
+        }
+    ],
+    "Other Channels": [
+        {
+            "channel_name": "Channel 4",
+            "link": "https://dlhd.so/embed/stream-354.php",
+        },
+        {
+            "channel_name": "Channel 5",
+            "link": "https://dlhd.so/embed/stream-355.php",
+        },
+        {
+            "channel_name": "CBS Sports Network",
+            "link": "https://dlhd.so/embed/stream-308.php",
+        },
+        {
+            "channel_name": "ESPN USA",
+            "link": "https://dlhd.so/embed/stream-44.php",
+        },
+        {
+            "channel_name": "ESPN2 USA",
+            "link": "https://dlhd.so/embed/stream-45.php",
+        },
+        {
+            "channel_name": "EuroSport 1",
+            "link": "https://dlhd.so/embed/stream-41.php",
+        },
+        {
+            "channel_name": "EuroSport 2",
+            "link": "https://dlhd.so/embed/stream-42.php",
+        },
+        {
+            "channel_name": "GOLF Channel USA",
+            "link": "https://dlhd.so/embed/stream-318.php",
+        },
+        {
+            "channel_name": "StarzPlay CricLife 1 HD",
+            "link": "https://dlhd.so/embed/stream-284.php",
+        },
+        {
+            "channel_name": "StarzPlay CricLife 2 HD",
+            "link": "https://dlhd.so/embed/stream-283.php",
+        },
+        {
+            "channel_name": "StarzPlay CricLife 3 HD",
+            "link": "https://dlhd.so/embed/stream-282.php",
+        },
+        {
+            "channel_name": "USA Network",
+            "link": "https://dlhd.so/embed/stream-343.php",
+        },
+        {
+            "channel_name": "Willow Cricket",
+            "link": "https://dlhd.so/embed/stream-346.php",
+        },
+        {
+            "channel_name": "Willow XTRA",
+            "link": "https://dlhd.so/embed/stream-598.php",
+        },
+        {
+            "channel_name": "WWE Network",
+            "link": "https://dlhd.so/embed/stream-376.php",
+        }
+    ]
+}
+  
+  const channel_buttons = [
+     "SuperSport",
+      "Sky Sports",
+      "TNT Sports",
+      "Astro Sports",
+      "Star Sports",
+      "Sony Sports",
+      "Bein Sports",
+      "Fox Sports",
+      "TNS",
+      "Football Clubs",
+      "Adults",
+      "Other Channels"
+  ]
+  
+  channel_buttons.forEach((e) => {
+   const container = document.querySelector('.channel-list');
+   container.innerHTML += `
+   <div class="channel" onclick="createChannelList('${e}', this)">${e}</div>
+   `;
+});
+
+// Toggle between light and dark mode
+    const toggleBtn = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    toggleBtn.addEventListener('click', () => {
+      if (body.classList.contains('light-mode')) {
+        body.classList.replace('light-mode', 'dark-mode');
+          toggleBtn.classList.add("active")
+      } else {
+        body.classList.replace('dark-mode', 'light-mode');
+          toggleBtn.classList.remove("active")
+      }
+    });
+
+function createChannelList(id, buttonElement) {
+   const channelListContainer = document.querySelector('.channel_list_container');
+   channelListContainer.innerHTML = "";
+   const channelData = data[id] || [];
+
+   // Remove 'active' class from all buttons
+   const allButtons = document.querySelectorAll('.channel');
+   allButtons.forEach(button => button.classList.remove('active'));
+   
+   // Add 'active' class to the clicked button
+   buttonElement.classList.add('active');
+
+   const channelInfo = channelData.map(channel => `
+       <button class="channel_list_link_button" onclick="changeIframeSource('${channel.link}', this)">
+           ${channel.channel_name}
+       </button>
+   `).join('');
+
+   channelListContainer.innerHTML = `<h3>${id}</h3>`;
+   const dataDiv = document.createElement('div');
+   dataDiv.className = 'data';
+   dataDiv.innerHTML = channelInfo;
+
+   channelListContainer.appendChild(dataDiv);
+}
+
+function changeIframeSource(link, buttonElement) {
+   const iframe = document.getElementById('stream-iframe');
+   iframe.src = link; // Update the iframe source to the clicked channel link
+
+   // Remove 'active' class from all channel list buttons
+   const allChannelButtons = document.querySelectorAll('.channel_list_link_button');
+   allChannelButtons.forEach(btn => btn.classList.remove('active'));
+
+   // Add 'active' class to the clicked button
+   buttonElement.classList.add('active');
+}
